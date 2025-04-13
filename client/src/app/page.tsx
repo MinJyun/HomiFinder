93c0ed1cf36562910d56415c5f52e6979e54597f
jhangminjyun <newmigan@gmail.com>
2025-04-13 13:04:11 +0800

    cleanup app

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Avatar } from '@/components/ui/avatar';
-import { Input } from '@/components/ui/input';
+import Navbar from "@/components/Navbar";
+import Landing from "./(nondashboard)/landing/page";
 
 export default function Home() {
   return (
-    <>
-      <div className="text-xl bg-black text-white">HOME</div>
-    </>
+    <div className="h-full w-full">
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}>
+        <Landing />
+      </main>
+    </div>
   );
 }
